ff4b2cd9f96032b41a3c51d503e09e29b43ce7b9
Hong JiHoo <gmacto123@gmail.com>
2019-07-17 13:23:44 +0900

    Updates

---
+++ precache-manifest.da9c937b71f541ea46dd534f69c6bda6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "398958610e804ec165988652efb84a06",
+    "url": "/react_mnist/index.html"
+  },
+  {
+    "revision": "f65299c4d12209ba6602",
+    "url": "/react_mnist/static/css/main.86eb330b.chunk.css"
+  },
+  {
+    "revision": "6559c094b014a99786b0",
+    "url": "/react_mnist/static/js/2.3d54403e.chunk.js"
+  },
+  {
+    "revision": "f65299c4d12209ba6602",
+    "url": "/react_mnist/static/js/main.52997f26.chunk.js"
+  },
+  {
+    "revision": "e94201de0ae67dcade77",
+    "url": "/react_mnist/static/js/runtime~main.3f8f0f9e.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/react_mnist/static/media/logo.ee7cd8ed.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react_mnist/precache-manifest.5b163f821c9abdba7a841ed22a49ddaf.js"
+  "/react_mnist/precache-manifest.da9c937b71f541ea46dd534f69c6bda6.js"
 );
 
 self.addEventListener('message', (event) => {
